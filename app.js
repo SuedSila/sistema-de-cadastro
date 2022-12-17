6c457bd54eb72ff2c6a7d4f3f41f727ec00f0ba1
SuedSila <117336594+SuedSila@users.noreply.github.com>
2022-12-17 18:52:34 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log("Rodando o sistema de gerenciamento de jogos")
+console.log(" Aplicação se conectando ao banco de dados")
+console.log("teste com o iF0rest")
+console.log("corrigindo o app js")
